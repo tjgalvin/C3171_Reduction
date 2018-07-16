d38885e65c735194730cf26c6ce5e44315e209c2
Tim Galvin <gal16b@csiro.au>
2018-07-16 15:12:02 +0000

    Added uvcat script

---
+++ build_uvcats.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+num_points=195
+outs=./Semester_2_Models
+for ((p=1;p<=num_points;p++))
+do
+	in7=Data/*/f7700_Model/c3171_$p.7700
+	in9=Data/*/f9500_Model/c3171_$p.9500
+	# in9=''
+	ins="$in7,$in9"
+	# for ((i=1;i<=num_days;i++))
+	# do
+	# 	ins=$ins,Night$i/Pointings/c3171_$p.7700,Night$i/Pointings/c3171_$p.9500
+	# done
+	echo $ins
+	uvcat vis=$ins out=$outs/c3171_$p.uv  &
+done
